f46c3edf3fdbff25794376c75c0667106fcfd1fc
Justagai <bguthrie24@outlook.com>
2018-11-30 21:05:46 -0800

    general bugfix patch for X-Wing

---
+++ xwing_Hook_General/Hook_General/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+[assembly: AssemblyTitle("XW General bugfix Hook")]
+[assembly: AssemblyDescription("Hook_General_Bugfix")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("XW General Bugfix Hook")]
+[assembly: AssemblyCopyright("Copyright © Blake Guthrie 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+
+[assembly: ComVisible(false)]
+
+
+[assembly: Guid("03be39c6-ccdd-41e6-b6f0-7fd9483d61e9")]
+
+
+[assembly: AssemblyVersion("1.0.4")]
+[assembly: AssemblyFileVersion("1.0.4")]
