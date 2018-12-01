3908133bb6004a7b3a497acdd5ae28527f20b917
Justagai <bguthrie24@outlook.com>
2018-11-30 21:33:13 -0800

    xwa tour multiplayer hook uploaded

---
+++ xwa_hook_tour_multiplayer/Hook_TourMultiplayer/Hooks.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Hook_TourMultiplayer
+{
+    static class Hooks
+    {
+        private static HookFunction[] hookFunctions = new HookFunction[]
+        {
+            //new HookFunction(0x43B81A, TourMultiplayer.TourMultiplayerHook)
+            new HookFunction(0x528BC0, TourMultiplayer.TourMultiplayerHook)
+        };
+
+        public static HookFunctionPtr[] hookFunctionsPtr = Array.ConvertAll(hookFunctions, t => t.Struct);
+
+        private static HookPatchItem[] tourMultiplayerPatch = new HookPatchItem[]
+        {
+            new HookPatchItem(0x127FBB, "BF602AAE00", "E860FF0700"),
+            new HookPatchItem(0x127FC0, "68201F6000", "E915A41600")
+        };
+
+        public static HookPatchItemPtr[] tourMultiplayerPatchPtr = Array.ConvertAll(tourMultiplayerPatch, t => t.Struct);
+
+        private static HookPatch[] patches = new HookPatch[]
+        {
+            new HookPatch("Tour Multiplayer Patch", tourMultiplayerPatchPtr),
+        };
+
+        public static HookPatchPtr[] patchesPtr = Array.ConvertAll(patches, t => t.Struct);
+    }
+}
