4b3af8114d814cbf209eb1cc98999c6c6f4d9666
Justagai <bguthrie24@outlook.com>
2018-11-30 20:53:01 -0800

    main X-Wing hook added

---
+++ xwing_hook_main/dinput/hooks.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "hook_function.h"
+
+static const HookPatchItem g_setupDInputHookPatch[] =
+{
+	{ 0x1A7F20, "00000000000000000000000000000000", "506033C054505050E8AB8FF0FF6158C3" },
+	{ 0x1A7F30, "00000000000000000000000000000000", "536033C054505050E89B8FF0FF615BC3" },
+};
+
+static const HookPatch g_patches[] =
+{
+	MAKE_HOOK_PATCH("To setup the dinput hook", g_setupDInputHookPatch),
+};
