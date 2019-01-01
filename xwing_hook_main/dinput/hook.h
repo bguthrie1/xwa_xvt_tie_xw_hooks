98d8438a79d2eead13284f4efb55071ceace0a1a
Justagai <bguthrie24@outlook.com>
2019-01-01 15:21:53 -0800

    Removed obsolete hooks

--- xwing_hook_main/dinput/hook.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "hook_function.h"
-
-void VirtualProtectHookMemory();
-
-bool PatchMemory(const HookPatch& patch);
-bool LoadAndPatchHooks();
-
-int Hook(int call, int* params);
